636858055a62904f4a88f6d54a2cb86e0ff53fb2
Maxime Béguin <maxime.beguin@veo-labs.com>
2020-05-04 09:34:30 +0200

    Auto-generated documentation for version 4.0.0

---
+++ 4.0.0/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AuthenticationError",
+        "OpenVeoClient",
+        "Request",
+        "RequestError",
+        "RestClient"
+    ],
+    "modules": [
+        "rest-client"
+    ],
+    "allModules": [
+        {
+            "displayName": "rest-client",
+            "name": "rest-client",
+            "description": "Defines a client to connect to OpenVeo web service."
+        }
+    ],
+    "elements": []
+} };
+});
